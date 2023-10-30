e596c5625f6502110b86261ebb6f2120ea7a188b
Racher <81470900+Racher612@users.noreply.github.com>
2023-10-30 23:11:20 +0300

    Test1 - понаставили знаков вопроса

--- app/src/main/java/com/example/homework1done/MainActivity.kt
+++ app/src/main/java/com/example/homework1done/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var grid : RecyclerView
+    private var grid : RecyclerView? = null
     private var orientation : Int = 0
     private var rectangles : MutableList<RectangleInfo> = mutableListOf()
     private var num : Int = 0
@@ -23,27 +23,27 @@ class MainActivity : AppCompatActivity() {
 
         grid = findViewById(R.id.GRID)
         if (orientation == Configuration.ORIENTATION_PORTRAIT)
-            grid.layoutManager = GridLayoutManager(this, 3) else
-            grid.layoutManager = GridLayoutManager(this, 4)
-        grid.adapter = CustomAdapter(rectangles)
+            grid?.layoutManager = GridLayoutManager(this, 3) else
+            grid?.layoutManager = GridLayoutManager(this, 4)
+        grid?.adapter = CustomAdapter(rectangles)
 
         val button : Button = findViewById(R.id.BUTTON)
         button.setOnClickListener {
             num += 1
             val rectangle = RectangleInfo(if (num % 2 == 0) R.color.red else R.color.blue, num)
             rectangles.add(rectangle)
-            (grid.adapter)?.notifyItemInserted(rectangles.size - 1)
+            (grid?.adapter)?.notifyItemInserted(rectangles.size - 1)
         }
     }
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
 
         if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-            grid.layoutManager = GridLayoutManager(this, 3)
+            grid?.layoutManager = GridLayoutManager(this, 3)
             orientation = Configuration.ORIENTATION_PORTRAIT
         }
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE){
-            grid.layoutManager = GridLayoutManager(this, 4)
+            grid?.layoutManager = GridLayoutManager(this, 4)
             orientation = Configuration.ORIENTATION_LANDSCAPE
         }
     }
